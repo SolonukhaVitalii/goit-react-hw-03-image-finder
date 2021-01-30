3fc6a80325d535ad7300c8384b3f32ec789a0695
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2021-01-30 17:04:53 +0200

    DZ3   work

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,37 +5,42 @@ import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    };
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    };
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
 
-    handleKeyDown = ({ code }) => {
-        const { onClose } = this.props;
-        code === 'Escape' && onClose();
-    };
+  handleKeyDown = ({ code }) => {
+    const { onClose } = this.props;
 
-    handleBackdropClick = ({ target, currentTarget }) => {
-        const { onClose } = this.props;
-        target === currentTarget && onClose();
-    };
-    
-    render() {
-        const { children } = this.props;
-        return createPortal(
-            <div className={s.Overlay} onClick={this.handleBackdropClick}>
-                <div className={s.Modal}>{children}</div>
-            </div>,
-            modalRoot,
-        );
-    };
-};
+    code === 'Escape' && onClose();
+  };
+
+  handleBackdropClick = ({ target, currentTarget }) => {
+    const { onClose } = this.props;
+
+    target === currentTarget && onClose();
+  };
+
+  render() {
+    const { children } = this.props;
+
+    return createPortal(
+      <div className={s.Overlay} onClick={this.handleBackdropClick}>
+        <div className={s.Modal}>{children}</div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
 
 Modal.propTypes = {
-    children: PropTypes.element.isRequired,
-    onClose: PropTypes.func.isRequired,  
-}
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default Modal;
